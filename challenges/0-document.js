45b32f8873d9603d689373f5a693b7f055c1fd74
Pamela Iman <pamelariman@gmail.com>
2024-05-30 13:37:36 +0100

    new repo init

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,15 @@
-// TODO (Ex.0): document and write tests for this function
+/**
+ * @function {asNegative} - writes out the input number as negative (if it isn't already)
+ * @param {number} n - input number
+ * @return {number} - n as a negative
+ */
 
 function asNegative(n) {
-  // ...
-}
+  if (n > 0) {
+    return -n
+  } else {
+    return n
+  }
+  }
 
-
+  asNegative(4)
