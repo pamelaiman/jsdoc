65af7fd2927a865bb6b803c5b0d4d62347594fe1
Pamela Iman <pamelariman@gmail.com>
2024-05-30 14:15:29 +0100

    added some test cases

--- examples/1-function-signatures.js
+++ examples/1-function-signatures.js
@@ -7,3 +7,25 @@
 ["a", "b", "c", "d"].includes();
 [1, 2, 3, 4].includes();
 ["a", "b", "c", "d"].push();
+
+// count must be 0 or above, if 0/empty then returns nothing. negative number will throw error
+console.log("hello ".repeat(2));
+console.log("hello ".repeat());
+
+// returns string in all upper case
+console.log("hello world".toUpperCase());
+
+// returns -1 if not found in string, or returns index number
+console.log("hello world".indexOf("f"));
+console.log("hello world".indexOf("e"));
+
+// returns boolean if found in string
+console.log(["a", "b", "c", "d"].includes("d"));
+console.log(["a", "b", "c", "d"].includes(2));
+
+console.log([1, 2, 3, 4].includes(2));
+console.log([1, 2, 3, 4].includes("hi"));
+
+// returns length of new array after push
+console.log(["a", "b", "c", "d"].push("e"));
+console.log(["a", "b", "c", "d"].push(1, 2, 3));
