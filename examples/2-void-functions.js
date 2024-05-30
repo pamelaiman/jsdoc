8dc3c8e3e6296708778981b83c2d3133d4b32a28
Pamela Iman <pamelariman@gmail.com>
2024-05-30 16:03:20 +0100

    fixed functions

--- examples/2-void-functions.js
+++ examples/2-void-functions.js
@@ -9,15 +9,12 @@ function addTwiceToArray(arrOfStrings, newString) {
   arrOfStrings.push(newString);
   arrOfStrings.push(newString);
 }
-
 console.log("\n------ addTwiceToArray -------");
 let arrOne = ['hi', 'hello'];
 console.log("Given arrOne", arrOne);
-console.log(
-  "After executing addTwiceToArray(arrOne, 'hey'):",
-  addTwiceToArray(arrOne, "hey")
-);
-console.log("Then it should have been mutated:", arrOne, "should be ['hi', 'hello', 'hey', 'hey']");
+// VOID: console.log("After executing addTwiceToArray(arrOne, 'hey'):", addTwiceToArray(arrOne, "hey"));
+addTwiceToArray(arrOne, 'hey')
+console.log("It has been changed to:", arrOne);
 
 
 /**
@@ -29,12 +26,9 @@ function orderByDescending(arrOfNums) {
   arrOfNums.sort((a, b) => a - b); // don't worry about the argument used here
   arrOfNums.reverse();
 }
-
 console.log("\n------ orderByDescending -------");
 let arrTwo = [3, 1, 2, 4];
 console.log("Given arrTwo is", arrTwo);
-console.log(
-  "After executing orderByDescending(arrTwo):",
-  orderByDescending(arrTwo)
-);
-console.log("Then it should have been mutated:", arrTwo, "should be [4, 3, 2, 1]");
+// VOID: console.log("After executing orderByDescending(arrTwo):",orderByDescending(arrTwo));
+orderByDescending(arrTwo)
+console.log("It has been changed to:", arrTwo);
